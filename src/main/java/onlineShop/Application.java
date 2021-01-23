6aa84bc16415a3fe62feb96fc6d098e228c8452e
Tianfeng Du <admiralight@gmail.com>
2021-01-23 13:55:49 -0500

    0.0.2

--- src/main/java/onlineShop/Application.java
+++ src/main/java/onlineShop/Application.java
@@ -1,10 +1,17 @@
 package onlineShop;
 
+import java.math.BigDecimal;
+
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import onlineShop.log.PaymentAction;
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
+        ApplicationContext container = new ClassPathXmlApplicationContext("payment.xml"); // container
+        PaymentAction paymentAction = (PaymentAction) container.getBean("paymentAction");
+        paymentAction.pay(new BigDecimal(2));
     }
 }
+
